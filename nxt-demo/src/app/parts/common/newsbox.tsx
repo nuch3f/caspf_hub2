130631d24b74d0d06846ffadc20a748a3ad5504e
forton <fortonspace@gmail.com>
2025-02-11 16:10:31 +0900

    250211 change api source

--- nxt-demo/src/app/parts/common/newsbox.tsx
+++ nxt-demo/src/app/parts/common/newsbox.tsx
@@ -12,7 +12,7 @@ type Post = {
   slug: string
 }
 
-const apiUrl = process.env.NEXT_PUBLIC_WP_API_DOMAIN + "/latestnews";
+const apiUrl = process.env.NEXT_PUBLIC_WP_API_DOMAIN + "/news";
 const fetchPosts = async (): Promise<Post[]> => {
   const res = await fetch(apiUrl)
   const data = await res.json()
@@ -36,7 +36,7 @@ const NewsBox = () => {
     <ul className="c-newsList">
         {posts.map((post: Post) => (
         <li className="c-newsList_item" key={post.id}>
-          <Link href={`/info/detail?pid=${post.slug}`} className="c-newsList_item--link">
+          <Link href={`/info/detail?pid=${post.id}`} className="c-newsList_item--link">
             <div className="c-newsList_item--container">
               <p className="c-newsList_item--info">
                 <time className="c-newsList_item--date">{post.date_gmt.slice(0,10)}</time>
